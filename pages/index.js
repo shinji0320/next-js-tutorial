984eb0f3b8fd901460a3815cf80389bf383b0927
shinji0320 <shinji.blue.one@gmail.com>
2021-03-24 09:10:06 +0900

    blog/show作成

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
-// export default function Home ({ allPostsData }) { ... }
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
@@ -20,7 +19,7 @@ export default function Home ({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>next.jsの学習中です。JavaScriptの種類多すぎ</p>
+        <p>next.jsの学習中です。JavaScript種類多すぎ</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
